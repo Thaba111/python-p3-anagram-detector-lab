6a97a1e7381f385ebac53998878f5a7e66fcdbcc
Edith Thaba <writersherry5@gmail.com>
2024-03-08 07:52:47 +0300

    Done with assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        anagrams = []
+
+        for w in words:
+            if sorted(w) == sorted(self.word) and w != self.word:
+                anagrams.append(w)
+
+        return anagrams
+
+
